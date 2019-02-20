7fbb4cbea7724ad4da1463f52dc8524663d2976c
Jille <jille.van.der.weerd@beeproger.com>
2019-02-20 10:33:12 +0100

    Initial Commit

---
+++ rsstalker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  rsstalker
+//
+//  Created by Jille van der Weerd on 20/02/2019.
+//  Copyright © 2019 jillevdweerd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
