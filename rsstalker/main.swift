dd9921ad9e911f64f6c7d7c90ddcfbab045d761e
JillevdW <Jillevdweerd@iCloud.com>
2019-02-28 13:46:29 +0100

    Main class handles commands

--- rsstalker/main.swift
+++ rsstalker/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
-
+if CommandLine.argc > 1 {
+    // Command was entered.
+    let first = CommandLine.arguments[1]
+    if first == "--help" {
+        print("Usage: ./rsstalker [username] [refreshtime in seconds (optional)]")
+    } else {
+        let username = CommandLine.arguments[1]
+        if CommandLine.argc > 2 {
+            if let timeInterval = Double(CommandLine.arguments[2]) {
+                print("Setting up stalker for \(username) to update once every \(Int(timeInterval)) seconds.")
+                Stalker.shared.setup(for: username, and: timeInterval)
+            } else {
+                print("Setting up stalker for \(username).")
+                Stalker.shared.setup(for: username)
+            }
+        } else {
+            print("Setting up stalker for \(username).")
+            Stalker.shared.setup(for: username)
+        }
+        RunLoop.main.run()
+    }
+} else {
+    print("Please run the '--help' command for information on how to use this tool.")
+}
